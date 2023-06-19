720c7a5d16ec54fc085c1a1f8e4808b39fd2f5cd
peng <562865683@qq.com>
2023-06-19 15:19:24 +0800

    [23节后]MyBatis入门

--- src/main/java/com/nowcoder/community/CommunityApplication.java
+++ src/main/java/com/nowcoder/community/CommunityApplication.java
@@ -1,7 +1,9 @@
 package com.nowcoder.community;
 
+import org.apache.ibatis.annotations.Mapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class CommunityApplication {
